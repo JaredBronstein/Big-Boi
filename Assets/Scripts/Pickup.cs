0f8da706a7c21b41612e4e74907161e3d13090c0
JaredBronstein <43156735+JaredBronstein@users.noreply.github.com>
2018-12-12 16:59:11 -0600

    Added Whole Project

---
+++ Assets/Scripts/Pickup.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            PlayerController.canDoubleJump = true;
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] //Always privatize variables and use SerializeField to have it still function as public for Unity
+    private Rigidbody2D myRigidbody;
+
+    [SerializeField]
+    private PhysicsMaterial2D playerMovingPhysicsMaterial, playerStoppingPhysicsMaterial;
+
+    [SerializeField]
+    private float accelerationForce = 5;
+
+    [SerializeField]
+    private float maxSpeed = 5;
+
+    [SerializeField]
+    private float jumpForce = 10;
+
+    [SerializeField]
+    private ContactFilter2D groundContactFilter;
+
+    [SerializeField]
+    private Collider2D groundDetectTrigger;
+
+    [SerializeField]
+    private Collider2D playerGroundCollider;
+
+    private bool isOnGround;
+    private float horizontalMovement;
+    private Collider2D[] groundHitDetectionResults = new Collider2D[16];
+    private bool isFacingRight = true;
+    private int Jumps = 0;
+
+    public static bool canDoubleJump = false;
+    void Update ()
+    {
+        UpdateIsOnGround();
+        HandleHorizontalInput();
+        HandleJumpInput();
+    }
+    private void UpdateIsOnGround()
+    {
+        isOnGround = groundDetectTrigger.OverlapCollider(groundContactFilter, groundHitDetectionResults) > 0;
+        if(canDoubleJump && isOnGround)
+        {
+            Jumps = 2;
+        }
+    }
+    private void HandleHorizontalInput()
+    {
+        horizontalMovement = Input.GetAxisRaw("Horizontal");
+    }
+    private void HandleJumpInput()
+    {
+        if (Input.GetButtonDown("Jump") && (isOnGround || Jumps > 0))
+        {
+            myRigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            if(Jumps > 0)
+            {
+                Jumps--;
+            }
+        }
+    }
+    private void FixedUpdate()
+    {
+        UpdatePhysicsMaterial();
+        Move();
+    }
+    private void UpdatePhysicsMaterial()
+    {
+        if (horizontalMovement == 0)
+        {
+            playerGroundCollider.sharedMaterial = playerStoppingPhysicsMaterial;
+        }
+        else
+        {
+            playerGroundCollider.sharedMaterial = playerMovingPhysicsMaterial;
+        }
+    }
+    private void Move()
+    {
+        myRigidbody.AddForce(Vector2.right * horizontalMovement * accelerationForce);
+        Vector2 clampedVelocity = myRigidbody.velocity;
+        clampedVelocity.x = Mathf.Clamp(myRigidbody.velocity.x, -maxSpeed, maxSpeed);
+        myRigidbody.velocity = clampedVelocity;
+    }
+}
